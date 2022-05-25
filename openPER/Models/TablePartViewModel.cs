b0fe9c5aabf881c83a9c7bfa13f5d23017cf5023
Chris Reynolds <chris@csjt.co.uk>
2022-05-25 22:49:05 +0100

    First version.  Rough shape of project and quick and dirty repository built

---
+++ openPER/Models/TablePartViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace openPER.Models
+{
+    public class TablePartViewModel
+    {
+        public string PartNumber { get; set; }
+        public string Description { get; set; }
+        public string Quantity { get; set; }
+        public int TableOrder { get; set; }
+    }
+}
