e50e709cde0fc60fbc87ba9af3c6af388b3b57a4
Chris Reynolds <chris@csjt.co.uk>
2022-05-28 19:38:17 +0100

    Filling out the parts table page and a little bit of styling

---
+++ openPER/Models/ModificationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace openPER.Models
+{
+    public class ModificationViewModel
+    {
+        public string Type { get; set; }
+        public int Code { get; set; }
+        public string Sequence { get; set; }
+        public string Description { get; set; }
+        public int Progression { get; set; }
+
+        public List<ActivationViewModel> Activations { get; set; }
+        public string FullDescription
+        {
+            get
+            {
+                var rc = Description;
+                foreach (var item in Activations)
+                {
+                    rc += $" {item.ActivationCode} {item.ActivationDescription} ({item.VariationType}{item.VariationCode} {item.VariationDescription} {item.OptionType}{item.OptionCode}{item.OptionDescription})";
+                }
+                return rc;
+            }
+        }
+
+    }
+}
--- openPER/Models/TablePartViewModel.cs
+++ openPER/Models/TablePartViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace openPER.Models
+﻿using System.Collections.Generic;
+
+namespace openPER.Models
 {
     public class TablePartViewModel
     {
         public double PartNumber { get; set; }
         public string Description { get; set; }
+        public string FurtherDescription { get; set; }
         public string Quantity { get; set; }
         public int TableOrder { get; set; }
+        public string Notes { get; set; }
+        public string Notes1 { get; set; }
+        public string Notes2 { get; set; }
+        public string Notes3 { get; set; }
+        public string Sequence { get; set; }
+        public List<ModificationViewModel> Modifications { get; set; }
+        public string Compatibility { get; internal set; }
     }
 }
