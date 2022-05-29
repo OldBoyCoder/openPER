9beb4fbe851077f9977c2d06345d41fe4530fc4d
Chris Reynolds <chris@csjt.co.uk>
2022-05-29 12:38:02 +0100

    Add search VIN facility, no styled UI yet

---
+++ openPER/Models/MvcViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace openPER.Models
+{
+    public class MvsViewModel
+    {
+        public string MvsCode { get; set; }
+        public string MvsVersion { get; set; }
+        public string MvsSeries { get; set; }
+
+        public string Description { get; set; }
+        public string SincomVersion { get; set; }
+        public string EngineType { get; set; }
+        public string EngineDescription { get; set; }
+        public string VariantDescription { get; set; }
+    }
+}
---
+++ openPER/Models/VinSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using VinSearcher;
+
+namespace openPER.Models
+{
+    public class VinSearchViewModel
+    {
+        public List<ModelViewModel> Models { get; set; }
+        public string SelectedModel { get; set; }
+        public int ChassisNumber { get; set; }
+        public VinResult Result { get; set; }
+        public MvsViewModel MvsData { get; set; }
+    }
+}
