ee9000a94c0716ec7c7ad156cb734dd011591788
Chris Reynolds <chris@csjt.co.uk>
2022-05-29 19:21:07 +0100

    Create search view component

---
+++ openPER/Views/Shared/Components/VinSearchWidget/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace openPER.Views.Shared.Components.VinSearchWidget
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
