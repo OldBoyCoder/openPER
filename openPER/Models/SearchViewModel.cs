c28b7af5914a2ee8b796c7a2661cfe99593b6468
Chris Reynolds <chris@csjt.co.uk>
2022-05-30 20:43:17 +0100

    Start search view component - issues with controller routing

---
+++ openPER/Models/SearchViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace openPER.Models
+{
+    public class SearchViewModel
+    {
+        public VinSearchViewModel VinSearch { get; set; }
+        public string PartNumber { get; set; }
+        public string FullVin { get; set; }
+        public SearchViewModel()
+        {
+            VinSearch = new VinSearchViewModel();
+        }
+    }
+}
---
+++ openPER/Views/Shared/Components/SearchWidget/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace openPER.Views.Shared.Components.SearchWidget
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
