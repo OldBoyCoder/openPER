20ff9560d5a9e4ce2ca5d3dcffe72e37589cf125
Chris Reynolds <chris@csjt.co.uk>
2022-05-31 19:26:44 +0100

    Change repository helper into extension method and change all repository code to use it

---
+++ openPER/Repositories/RepositoryHelpers.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace openPER.Repositories
+{
+    internal static class RepositoryHelpers
+    {
+        internal static void RunSqlAllRows(this SqliteConnection connection, string sql, Action<SqliteDataReader> rowHandler, params object[] parameters)
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = sql;
+            if (parameters != null)
+            {
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    command.Parameters.AddWithValue("$p" + (i + 1).ToString(), parameters[i]);
+                }
+            }
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    rowHandler(reader);
+                }
+            }
+
+        }
+        internal static void RunSqlFirstRowOnly(this SqliteConnection connection, string sql, Action<SqliteDataReader> rowHandler, params object[] parameters)
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = sql;
+            if (parameters != null)
+            {
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    command.Parameters.AddWithValue("$p" + (i + 1).ToString(), parameters[i]);
+                }
+            }
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    rowHandler(reader);
+                }
+            }
+
+        }
+
+    }
+}
