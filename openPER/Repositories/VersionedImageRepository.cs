f0aa7c417d60e8ab099bb8cd2c3a3e8f2457a959
Chris Reynolds <chris@csjt.co.uk>
2022-06-02 16:06:25 +0100

    Create versioned image repository (only R18 at present)

---
+++ openPER/Repositories/VersionedImageRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using openPER.Interfaces;
+namespace openPER.Repositories
+{
+    public class VersionedImageRepository : IVersionedImageRespository
+    {
+        IImageRepository _repository18;
+        IImageRepository _repository84;
+        public VersionedImageRepository(IConfiguration config)
+        {
+            _repository18 = new Release18ImageRepository(config);
+        }
+        public byte[] GetImageForCatalogue(int releaseCode, string cmgCode)
+        {
+            switch (releaseCode)
+            {
+                case 18:
+                    return _repository18.GetImageForCatalogue(cmgCode);
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
