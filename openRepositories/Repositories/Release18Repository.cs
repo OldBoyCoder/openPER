3cc74b48c6ccf199e6d5953f08bc64f505e0a173
Chris Reynolds <chris@csjt.co.uk>
2022-06-09 16:38:23 +0100

    Move getting of catalogue hotspots to release specific class to cope with misaligned hotspots.  Fixes #1

--- openRepositories/Repositories/Release18Repository.cs
+++ openRepositories/Repositories/Release18Repository.cs
@@ -39,5 +39,30 @@ namespace openPERRepositories.Repositories
             imageName = "";
             return map;
         }
+        public override List<GroupImageMapEntryModel> GetGroupMapEntriesForCatalogue(string catalogueCode)
+        {
+            var map = new List<GroupImageMapEntryModel>();
+            using var connection = new SqliteConnection($"Data Source={_pathToDb}");
+            var sql = @"select MPG_TX-MPI_OFFX, MPG_TY-MPI_OFFY, MPG_INDEX, GRP_COD from CATALOGUES C
+                        JOIN MAP_GRP M ON M.MAP_NAME = C.MAP_NAME
+                        JOIN MAP_INFO MI ON MI.MAP_NAME = C.MAP_NAME
+                        WHERE C.CAT_COD = $p1
+                            AND M.GRP_COD IN (SELECT DISTINCT GRP_COD FROM TBDATA WHERE CAT_COD = $p1)
+                        ORDER BY MPG_TY, MPG_TX, MPG_INDEX";
+            connection.RunSqlAllRows(sql, (reader) =>
+            {
+                var m = new GroupImageMapEntryModel
+                {
+                    X = reader.GetInt32(0),
+                    Y = reader.GetInt32(1),
+                    Index = reader.GetInt32(2),
+                    GroupCode = reader.GetInt32(3)
+                };
+                map.Add(m);
+            }, catalogueCode);
+            return map;
+
+        }
+
     }
 }
