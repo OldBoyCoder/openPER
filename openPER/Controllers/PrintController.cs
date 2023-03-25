21573b18de021897a9f5dda31d67778163354faf
OldBoyCoder <chris@csjt.co.uk>
2023-03-25 08:17:00 +0000

    Addition of Print Controller and supporting class library

---
+++ openPER/Controllers/PrintController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using openPERRepositories.Interfaces;
+
+namespace openPER.Controllers
+{
+    public class PrintController : Controller
+    {
+        readonly IRepository _rep;
+        readonly IMapper _mapper;
+
+        public PrintController(IRepository rep, IMapper mapper)
+        {
+            _rep = rep;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            var x = new openPERPrinting.PdfPrint(_rep);
+            var fileStream = x.CreateDocument();
+            return File(fileStream, "application/pdf", "MyDocument.PDF");
+        }
+    }
+}
---
+++ openPERPrinting/PdfPrint.cs
@@ -0,0 +1,28 @@
+﻿using openPERRepositories.Interfaces;
+using PdfSharpCore.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openPERPrinting
+{
+    public class PdfPrint
+    {
+        IRepository _rep;
+        public PdfPrint(IRepository rep)
+        {
+            _rep = rep;
+        }
+
+        public Stream CreateDocument()
+        {
+            var doc = new PdfDocument();
+            var p = doc.AddPage();
+            var ms = new MemoryStream();
+            doc.Save(ms, false);
+            return ms;
+        }
+    }
+}
